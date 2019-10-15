83093f77f153c0d825f5c07e56003184030c4245
yujie-tao <yujietao@outlook.com>
2019-10-14 21:37:25 -0400

    Revert "classifier done"
    This reverts commit 6098fcfa3dead55da45f5c907a2aae96ebdc60c2.

--- classification.py
+++ classification.py
@@ -3,22 +3,20 @@ import numpy as np
 from sklearn import svm
 
 def main():
-	classifier = train()
-	
-
-
-def train():
 	palm_data = load_data('palm.txt')
 	stone_data = load_data('stone.txt')
-	train_data = np.concatenate([palm_data, stone_data])
+	train_data = palm_data + stone_data
+	print(train_data)
 
 	palm_label = [0 for i in range(len(palm_data))]
-	stone_label = [1 for i in range(len(stone_data))]
+	stone_label = [0 for i in range(len(stone_data))]
 	train_label = palm_label + stone_label
 
-	classifier = svm.SVC(gamma='scale')
-	classifier.fit(train_data, train_label)  
-	return classifier
+	clf = svm.SVC(gamma='scale')
+	clf.fit(train_data, train_label)  
+
+
+
 
 
 
@@ -37,11 +35,8 @@ def load_data(file_name):
 			if end_index != len(elements)-1:
 				elements = elements[0:end_index]
 			break
-
-	# Get rid of '\n' and all 'end' elements
 	elements_arr = [i.strip() for i in elements if "end" not in i]
-	# Split into chunks with each of 21
-	elements_chunk = np.array_split(elements_arr, len(elements_arr)/21)
+	elements_chunk = np.array_split(elements_arr, len(elements)/21)
 
 	output = []
 
@@ -50,12 +45,10 @@ def load_data(file_name):
 		new_chunk = [i.split(',') for i in chunk]
 		dstring_chunk = []
 		for coord in new_chunk:
-			dstring = np.asarray([float(i) for i in coord])
+			dstring = [float(i) for i in coord]
 			dstring_chunk.append(dstring)
 
-		output.append(np.asarray(dstring_chunk))
-
-	output = np.asarray(output).reshape(len(output)*21,2)
+		output.append(dstring_chunk)
 	
 	return output
 
