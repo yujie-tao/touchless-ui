8168e2deeea9d828fc60e26085dc690745a5f616
yujie-tao <yujietao@outlook.com>
2019-10-15 11:42:45 -0400

    fix bug in classification, not works

--- classification.py
+++ classification.py
@@ -5,24 +5,33 @@ from sklearn import svm
 
 def main():
 	classifier = train()
-	print('classifier done')
+	# a = np.asarray([0.513122,0.860783,0.356239,0.790055,0.274009,0.668277,0.246593,0.552753,0.221277,0.469811,0.414848,0.50565 ,0.387338,0.34371 ,0.377919,0.23178,0.373127,0.143402,0.520822,0.508402,0.538382,0.331114,0.547799,0.214045,0.545361,0.124099,0.613324,0.541049,0.643841,0.379376,0.645113,0.273412,0.638148,0.187606,0.703423,0.596537,0.745857,0.481994,0.779236,0.391669,0.80264,0.2922842]).reshape(1,42)
+	# print((np.asarray(a)).shape)
+	# print(classifier.predict(a))
+	# print('classifier done')
 	file = open("mediapipe/test.txt","r")
 	gesture_stream = follow(file)
-	for gesture in gesture_stream:
-		if "end" not in gesture:
-			gesture = gesture.strip().split(',')
-			gesture = [[float(i) for i in gesture]]
-			print(classifier.predict(gesture))
+	gesture = []
+	for finger_coord in gesture_stream:
+		if "end" not in finger_coord:
+			finger_coord = finger_coord.strip().split(',')
+			finger_coord = [float(i) for i in finger_coord]
+			gesture.extend(finger_coord)
+
+			if len(gesture) == 42:
+				test_gesture = np.asarray(gesture).reshape(1,42)
+				print(classifier.predict(test_gesture))
+				gesture.clear()
 
 
 def follow(file_name):
     file_name.seek(0,2)
     while True:
-        gesture = file_name.readline()
-        if not gesture:
+        finger_coord = file_name.readline()
+        if not finger_coord:
             time.sleep(0.1)
             continue
-        yield gesture
+        yield finger_coord
 
 
 def train():
@@ -32,7 +41,7 @@ def train():
 
 	palm_label = [0 for i in range(len(palm_data))]
 	stone_label = [1 for i in range(len(stone_data))]
-	train_label = palm_label + stone_label
+	train_label = np.concatenate([palm_label,stone_label])
 
 	classifier = svm.SVC(gamma='scale')
 	classifier.fit(train_data, train_label)  
@@ -71,9 +80,11 @@ def load_data(file_name):
 			dstring = np.asarray([float(i) for i in coord])
 			dstring_chunk.append(dstring)
 
-		output.append(np.asarray(dstring_chunk))
+		dstring_chunk = (np.asarray(dstring_chunk)).reshape(1,42)
+		output.append(dstring_chunk)
 
-	output = np.asarray(output).reshape(len(output)*21,2)
+
+	output = np.asarray(output).reshape(len(output),42)
 	
 	return output
 
